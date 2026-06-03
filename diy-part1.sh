a83b712721f784e7f58e64cb3f5f3991489bfcce
GA-LSQ <70734094+GA-LSQ@users.noreply.github.com>
2026-06-03 21:25:15 +0800

    Add git clone commands for bandix packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
+git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
+git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
