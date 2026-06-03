94dd0d46e08d70ea02e9538781fd0f25cb51771f
GA-LSQ <70734094+GA-LSQ@users.noreply.github.com>
2026-06-03 21:16:55 +0800

    Change default IP and modify hostname to Cudy

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,13 +17,13 @@ git clone https://github.com/theosoft-git/luci-app-easymesh.git package/luci-app
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/LEDE/iStoreOS/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/iStoreOS/g' package/base-files/files/etc/init.d/system
-sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/LEDE/iStoreOS/g' package/base-files/luci2/bin/config_generate
-sed -i 's/LEDE/iStoreOS/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/LEDE/Cudy/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/Cudy/g' package/base-files/files/etc/init.d/system
+sed -i 's/LEDE/Cudy/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/LEDE/Cudy/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/Cudy/g' package/lean/default-settings/files/zzz-default-settings
 
 curl -o package/base-files/files/etc/banner https://raw.githubusercontent.com/istoreos/istoreos/refs/heads/istoreos-22.03/package/base-files/files/etc/banner
