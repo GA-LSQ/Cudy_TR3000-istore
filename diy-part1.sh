b48b9eb1a21d9fa9c17b6700e30cd09facbaf0ed
GA-LSQ <70734094+GA-LSQ@users.noreply.github.com>
2026-06-07 00:15:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@ echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # 解决rust相关报错
